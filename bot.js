14654ad5a1fcc5c8290da050f2cf97ff8762621f
Maurici0 <info@mauricestolk.nl>
2022-04-11 20:43:10 +0200

    Fusing theshake-bot

--- bot.js
+++ bot.js
@@ -1,417 +1,384 @@
-const { subtle } = require('crypto').webcrypto;
+( () => {
+	const { subtle } = require('crypto').webcrypto;
 
-var http = require("https");
-var WebSocket = require("ws");
+	const xml2js = require('xml2js');
+	
+	const http = require("https");
+	const WebSocket = require("ws");
+	const { session, domain, keys, conversation, trigger } = require("./config.json");
 
-let config = require("./config.json");
-let session = config.session;
-let domain = config.domain;
-let keys = config.keys;
-let conversation = config.conversation;
+	let socket;
+	let users,conversations;
 
-let trigger = config.trigger;
+	getUsers().then(r => {
+		users = r.users;
 
-var socket;
-var users,conversations;
+		getConversations().then(r => {
+			conversations = r.conversations;
 
-getUsers().then(r => {
-	users = r.users;
-
-	getConversations().then(r => {
-		conversations = r.conversations;
-
-		var pms = 0;
-		Object.keys(conversations).forEach(c => {
-			if (!conversations[c].group) {
-				pms += 1;
-			}
-		});
-
-		var ready = 0;
-		Object.keys(conversations).forEach(c => {
-			makeSecretIfNeeded(c).then(d => {
+			let pms = 0;
+			Object.keys(conversations).forEach(c => {
 				if (!conversations[c].group) {
-					ready += 1;
-				}
-
-				if (ready == pms) {
-					setupWebSocket();
+					pms += 1;
 				}
 			});
-		});
-	});
-});
 
-function log(m) {
-	console.log(m);
-}
+			let ready = 0;
+			Object.keys(conversations).forEach(c => {
+				makeSecretIfNeeded(c).then(d => {
+					if (!conversations[c].group) {
+						ready += 1;
+					}
 
-function setupWebSocket() {
-	if (!socket) {
-		socket = new WebSocket("wss://ws.hns.chat");
-
-		socket.onopen = e => {
-			socket.send("IDENTIFY "+session);
-		};
-
-		socket.onmessage = e => {
-			parse(e);
-		};
-
-		socket.onclose = e => {
-			socket = false;
-		};
-	}
-}
-
-function isGroup(id) {
-	try {
-		if (conversations[id].group) {
-			return true;
-		}
-	}
-	catch (error) {}
-	return false;
-}
-
-async function messageBody(message) {
-	let output = new Promise(function(resolve){
-		if (isGroup(message.conversation)) {
-			resolve(message.message);
-		}
-		else {
-			let dkey = conversations[message.conversation].key;
-			decryptMessage(message.message, dkey, message.conversation).then(function(decoded){
-				resolve(decoded);
-			});
-		}
-	});
-
-	return await output;
-}
-
-function parse(e) {
-	let message = e.data;
-	let split = message.match(/(?<command>[A-Z]+)\s(?<body>.+)/);
-	let command = split.groups.command;
-	let body = JSON.parse(split.groups.body);
-
-	switch (command) {
-		case "MESSAGE":
-			if (body.user === domain) {
-				return;
-			}
-
-			if (conversation && body.conversation !== conversation) {
-				return;
-			}
-
-			if (Object.keys(conversations).includes(body.conversation)) {
-				messageBody(body).then(decoded => {
-					if (decoded[0] === trigger) {
-						handleCommand(body, decoded);
+					if (ready == pms) {
+						setupWebSocket();
 					}
 				});
-			}
-			break;
-
-		case "CONVERSATION":
-			conversations[conversation.id] = body;
-			makeSecret(body);
-			break;
-	}
-}
-
-function handleCommand(msg, message) {
-	let split = message.split(" ");
-	let command = split[0].substring(1);
-	split.shift();
-	let params = split;
-
-	switch (command) {
-		case "hns":
-			hnsPrice().then(response => {
-				if (response) {
-					reply(msg, "$"+response);
-				}
 			});
-			break;
+		});
+	});
 
-		case "theshake":
-			getShakeFeed().then(response=>{
-				if (response) {
-					let parser = new xml2js.Parser();
-					parser.parseStringPromise(response).then(function (result) {
-						const item = result.rss.channel[0].item[0];
-						const { title, description, link } = item;
-						const article = `${title}\n${description}\n${link}`;
-						reply(body,article);
-						parser.
-					}).catch(err=>{
-						// Failed to parse XML
+	function log(m) {
+		console.log(m);
+	}
+
+	function setupWebSocket() {
+		if (!socket) {
+			socket = new WebSocket("wss://ws.hns.chat");
+
+			socket.onopen = e => {
+				socket.send("IDENTIFY "+session);
+			};
+
+			socket.onmessage = e => {
+				parse(e);
+			};
+
+			socket.onclose = e => {
+				socket = false;
+			};
+		}
+	}
+
+	function isGroup(id) {
+		try {
+			if (conversations[id].group) {
+				return true;
+			}
+		}
+		catch (error) {}
+		return false;
+	}
+
+	async function messageBody(message) {
+		return await new Promise(function(resolve){
+			if (isGroup(message.conversation)) {
+				resolve(message.message);
+			}
+			else {
+				let dkey = conversations[message.conversation].key;
+				decryptMessage(message.message, dkey, message.conversation).then(function(decoded){
+					resolve(decoded);
+				});
+			}
+		});
+	}
+
+	function parse(e) {
+		const message = e.data;
+		const split = message.match(/(?<command>[A-Z]+)\s(?<body>.+)/);
+		const command = split.groups.command;
+		const body = JSON.parse(split.groups.body);
+
+		switch (command) {
+			case "MESSAGE":
+				if (body.user === domain) return;
+				
+				if (conversation && body.conversation !== conversation) return;
+				
+				if (Object.keys(conversations).includes(body.conversation)) {
+					messageBody(body).then(decoded => {
+						if (decoded[0] === trigger) {
+							handleCommand(body, decoded);
+						}
 					});
 				}
-			}).catch(err=>{
-				// Failed to fetch
-			});
-			break;
-		
-		default: break;
-	}
+				break;
 
-}
-
-function reply(message, string) {
-	let dkey = conversations[message.conversation].key || null;
-	encryptIfNeeded(message.conversation, string, dkey).then(function(m){
-		let data = {
-			action: "sendMessage",
-			conversation: message.conversation,
-			from: domain,
-			message: m
-		};
-
-		ws("ACTION", data);
-	});
-}
-
-async function encryptIfNeeded(conversation, message, dkey) {
-	let output = new Promise(function(resolve) {
-		if (dkey) {
-			encryptMessage(message, dkey, conversation).then(function(m){
-				resolve(m);
-			});
+			case "CONVERSATION":
+				conversations[conversation.id] = body;
+				makeSecret(body);
+				break;
 		}
-		else {
-			resolve(message);
+	}
+
+	function handleCommand(msg, message) {
+		let split = message.split(" ");
+		const command = split[0].substring(1);
+		split.shift();
+		const params = split;
+
+		switch (command) {
+			case "hns":
+				fetchData({
+					host: "api.coingecko.com",
+					path: "/api/v3/simple/price?ids=handshake&vs_currencies=usd",
+				}).then(response => {
+					if (response) {
+						const json = JSON.parse(response);
+						reply(msg, `$${json.handshake.usd}`);
+					}
+				});
+				break;
+
+			case "theshake":
+				fetchData({
+					host: "theshake.substack.com", 
+					path: "/feed"
+				}).then(response => {
+		  		if (response) {	
+		  			parseXML(response).then(feed => {
+		  				const latest = feed.rss.channel[0].item[0];
+			  			const { title, description, link } = latest;
+							reply(msg, `${link}`);
+		  			});
+		  		}
+				});
+				break;
+			
+			default: break;
 		}
-	});
 
-	return await output;
-}
-
-function ws(command, body) {
-	socket.send(command+" "+JSON.stringify(body));
-}
-
-async function hnsPrice() {
-	let options = {
-		host: "api.coingecko.com",
-		path: "/api/v3/simple/price?ids=handshake&vs_currencies=usd",
 	}
 
-	let output = new Promise(resolve => {
-		http.get(options, r => {
-			var response = '';
-			
-			r.on('data', chunk => {
-				response += chunk;
-			});
-			r.on('end', () => {
-				let json = JSON.parse(response);
-				resolve(json.handshake.usd);
-			});
-		}).on('error', e => {
-			resolve();
+	function reply(message, string) {
+		const dkey = conversations[message.conversation].key || null;
+		encryptIfNeeded(message.conversation, string, dkey).then(function(m){
+			const data = {
+				action: "sendMessage",
+				conversation: message.conversation,
+				from: domain,
+				message: m
+			};
+
+			ws("ACTION", data);
 		});
-	});
-
-	return await output;
-}
-
-async function getShakeFeed() {
-	let options = {
-		host: "theshake.substack.com",
-		path: "/feed",
 	}
 
-	let output = new Promise(resolve => {
-		http.get(options, r => {
-			let response = '';
-			
-			r.on('data', chunk => {
-				response += chunk;
+	async function parseXML(data) {
+		const parser = new xml2js.Parser();
+		return await new Promise(resolve => {
+			parser.parseStringPromise(data).then(result => {
+				resolve(result);				
+			}).catch(err => {
+				log(err);
+				resolve(err);
 			});
-			r.on('end', () => {
-				resolve(response);
-			});
-		}).on('error', e => {
-			resolve();
 		});
-	});
+	};
 
-	return await output;
-}
-
-async function api(data) {
-	if (session) {
-		data["key"] = session;
-	}
-
-	let output = new Promise(resolve => {
-		data = JSON.stringify(data);
-
-		let options = {
-			host: "hns.chat",
-			path: "/api",
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-		        'Content-Length': Buffer.byteLength(data)
+	async function encryptIfNeeded(conversation, message, dkey) {
+		return await new Promise(resolve => {
+			if (dkey) {
+				encryptMessage(message, dkey, conversation).then(function(m){
+					resolve(m);
+				});
 			}
-		}
-
-		var req = http.request(options, r => {
-			var response = '';
-			r.on('data', chunk => {
-				response += chunk;
-			});
-			r.on('end', () => {
-				let json = JSON.parse(response);
-				resolve(json);
-			});
-		}).on('error', e => {
-			resolve();
+			else {
+				resolve(message);
+			}
 		});
+	}
 
-		req.write(data);
-		req.end();
-	});
+	function ws(command, body) {
+		socket.send(command+" "+JSON.stringify(body));
+	}
 
-	return await output;
-}
-
-async function makeSecretIfNeeded(c) {
-	let output = new Promise(resolve => {
-		if (!conversations[c].group) {
-			makeSecret(c).then(r => {
+	async function fetchData(options) {
+		return await new Promise(resolve => {
+			http.get(options, r => {
+				let data = '';
+				
+				r.on('data', chunk => {
+					data += chunk;
+				});
+				r.on('end', () => {
+					resolve(data);
+				});
+			}).on('error', e => {
 				resolve();
 			});
+		});
+	}
+
+
+	async function api(data) {
+		if (session) {
+			data["key"] = session;
 		}
-		else {
-			resolve();
-		}
-	});
 
-	return await output;
-}
+		return await new Promise(resolve => {
+			data = JSON.stringify(data);
 
-async function makeSecret(k) {
-	let derivedKey = new Promise(resolve => {
-		let otherUser = getOtherUser(k);
-		let otherKey = JSON.parse(otherUser.pubkey);
+			const options = {
+				host: "hns.chat",
+				path: "/api",
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+			        'Content-Length': Buffer.byteLength(data)
+				}
+			}
 
-		if (otherKey) {
-			deriveKey(otherKey, keys.privateKeyJwk).then(d => {
-				conversations[k].key = d;
-				resolve(d);
+			const req = http.request(options, r => {
+				let response = '';
+				r.on('data', chunk => {
+					response += chunk;
+				});
+				r.on('end', () => {
+					const json = JSON.parse(response);
+					resolve(json);
+				});
+			}).on('error', e => {
+				resolve();
 			});
-		}
-		else {
-			resolve();
-		}
-	}); 
 
-	return await derivedKey;
-}
+			req.write(data);
+			req.end();
+		});
+	}
 
-function getOtherUser(id) {
-	let c = conversations[id];
+	async function makeSecretIfNeeded(c) {
+		return await new Promise(resolve => {
+			if (!conversations[c].group) {
+				makeSecret(c).then(r => {
+					resolve();
+				});
+			}
+			else {
+				resolve();
+			}
+		});
+	}
 
-	let user = Object.keys(c.users).filter(user => {
-		return user !== domain;
-	}).join(", ");
+	async function makeSecret(k) {
+		let derivedKey = new Promise(resolve => {
+			let otherUser = getOtherUser(k);
+			let otherKey = JSON.parse(otherUser.pubkey);
 
-	return c.users[user];
-}
+			if (otherKey) {
+				deriveKey(otherKey, keys.privateKeyJwk).then(d => {
+					conversations[k].key = d;
+					resolve(d);
+				});
+			}
+			else {
+				resolve();
+			}
+		}); 
 
-function getUsers() {
-	let data = {
-		action: "getUsers"
+		return await derivedKey;
+	}
+
+	function getOtherUser(id) {
+		const c = conversations[id];
+
+		const user = Object.keys(c.users)
+		 .filter(u => u !== domain)
+		 .join(", ");
+
+		return c.users[user];
+	}
+
+	function getUsers() {
+		const data = {
+			action: "getUsers"
+		};
+
+		return api(data);
+	}
+
+	function getConversations() {
+		const data = {
+			action: "getConversations",
+			domain: domain
+		};
+
+		return api(data);
+	}
+
+	async function deriveKey(publicKeyJwk, privateKeyJwk) {
+	  const publicKey = await subtle.importKey(
+	    "jwk",
+	    publicKeyJwk,
+	    {
+	      name: "ECDH",
+	      namedCurve: "P-256",
+	    },
+	    true,
+	    []
+	  );
+
+	  const privateKey = await subtle.importKey(
+	    "jwk",
+	    privateKeyJwk,
+	    {
+	      name: "ECDH",
+	      namedCurve: "P-256",
+	    },
+	    true,
+	    ["deriveKey", "deriveBits"]
+	  );
+
+	  return await subtle.deriveKey(
+	    { name: "ECDH", public: publicKey },
+	    privateKey,
+	    { name: "AES-GCM", length: 256 },
+	    true,
+	    ["encrypt", "decrypt"]
+	  );
 	};
 
-	return api(data);
-}
+	async function encryptMessage(text, derivedKey, conversation) {
+	  const encodedText = new TextEncoder().encode(text);
 
-function getConversations() {
-	let data = {
-		action: "getConversations",
-		domain: domain
+	  const encryptedData = await subtle.encrypt(
+	    { name: "AES-GCM", iv: new TextEncoder().encode(conversation) },
+	    derivedKey,
+	    encodedText
+	  );
+
+	  const uintArray = new Uint8Array(encryptedData);
+
+	  const string = String.fromCharCode.apply(null, uintArray);
+
+	  const base64Data = btoa(string);
+
+	  return base64Data;
 	};
 
-	return api(data);
-}
+	async function decryptMessage(text, derivedKey, conversation) {
+	  try {
+	    const initializationVector = new Uint8Array(new TextEncoder().encode(conversation)).buffer;
 
-async function deriveKey(publicKeyJwk, privateKeyJwk) {
-  const publicKey = await subtle.importKey(
-    "jwk",
-    publicKeyJwk,
-    {
-      name: "ECDH",
-      namedCurve: "P-256",
-    },
-    true,
-    []
-  );
+	    const string = atob(text);
+	    const uintArray = new Uint8Array(
+	      [...string].map((char) => char.charCodeAt(0))
+	    );
+	    const algorithm = {
+	      name: "AES-GCM",
+	      iv: initializationVector,
+	    };
+	    const decryptedData = await subtle.decrypt(
+	      algorithm,
+	      derivedKey,
+	      uintArray
+	    );
 
-  const privateKey = await subtle.importKey(
-    "jwk",
-    privateKeyJwk,
-    {
-      name: "ECDH",
-      namedCurve: "P-256",
-    },
-    true,
-    ["deriveKey", "deriveBits"]
-  );
+	    return new TextDecoder().decode(decryptedData);
+	  } catch (e) {
+	    return text;
+	  }
+	};
 
-  return await subtle.deriveKey(
-    { name: "ECDH", public: publicKey },
-    privateKey,
-    { name: "AES-GCM", length: 256 },
-    true,
-    ["encrypt", "decrypt"]
-  );
-};
-
-async function encryptMessage(text, derivedKey, conversation) {
-  const encodedText = new TextEncoder().encode(text);
-
-  const encryptedData = await subtle.encrypt(
-    { name: "AES-GCM", iv: new TextEncoder().encode(conversation) },
-    derivedKey,
-    encodedText
-  );
-
-  const uintArray = new Uint8Array(encryptedData);
-
-  const string = String.fromCharCode.apply(null, uintArray);
-
-  const base64Data = btoa(string);
-
-  return base64Data;
-};
-
-async function decryptMessage(text, derivedKey, conversation) {
-  try {
-    const initializationVector = new Uint8Array(new TextEncoder().encode(conversation)).buffer;
-
-    const string = atob(text);
-    const uintArray = new Uint8Array(
-      [...string].map((char) => char.charCodeAt(0))
-    );
-    const algorithm = {
-      name: "AES-GCM",
-      iv: initializationVector,
-    };
-    const decryptedData = await subtle.decrypt(
-      algorithm,
-      derivedKey,
-      uintArray
-    );
-
-    return new TextDecoder().decode(decryptedData);
-  } catch (e) {
-    return text;
-  }
-};
+})();
